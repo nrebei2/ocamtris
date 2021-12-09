61dad2a34b761bc8209fdf04d5f25799ea47b36c
Noah Solomon <njs99@cornell.edu>
2021-12-09 17:22:17 -0500

    changed more specs'

--- src/Startgame.mli
+++ src/Startgame.mli
@@ -1,34 +1,33 @@
+type timers = {
+  mutable update : float;
+  mutable drop_timer : float;
+  mutable bot_timer : float;
+}
 (* mutable state of [game.timer]. [timers.update] is the interval in
-  seconds from the last frame to the current one. [timers.drop_timer]
-  is the interval in seconds from the last call of moving the pieces
-  down. [timers.bot_timer] is the interval in seconds from the bot's
-  last update *)
-  type timers = {
-    mutable update : float;
-    mutable drop_timer : float;
-    mutable bot_timer : float;
-   }
-    
-   (* mutable state of the active game. [game.over] is [true] if the game
-     is over, [false] otherwise. [game.players] is the gams's players.
-     [game.gravity] corresponds to how fast the pieces move down.
-     [game.difficulty] is a chosen difficulty by the user, corresponds to
-     how fast the bot is. [game.timers] are the timers for the game *)
-   type game = {
-    mutable over : bool;
-    mutable players : Player.player list;
-    mutable gravity : float;
-    mutable difficulty : float;
-    mutable timers : timers;
-   } 
-    
-   (* [default_timer ()] returns [timer] where all fields are initialized
-     as [0.] *)
-   val default_timer : unit -> timers
-    
-   (* current game the player is in*)
-   val cur_game : game
-    
-   (* [run game] starts the game given [game]'s specifications *)
-   val play_game : unit -> unit
-   
+   seconds from the last frame to the current one. [timers.drop_timer]
+   is the interval in seconds from the last call of moving the pieces
+   down. [timers.bot_timer] is the interval in seconds from the bot's
+   last update *)
+
+type game = {
+  mutable over : bool;
+  mutable players : Player.player list;
+  mutable gravity : float;
+  mutable difficulty : float;
+  mutable timers : timers;
+}
+(* mutable state of the active game. [game.over] is [true] if the game
+   is over, [false] otherwise. [game.players] is the gams's players.
+   [game.gravity] corresponds to how fast the pieces move down.
+   [game.difficulty] is a chosen difficulty by the user, corresponds to
+   how fast the bot is. [game.timers] are the timers for the game *)
+
+val default_timer : unit -> timers
+(* [default_timer ()] returns [timer] where all fields are initialized
+   as [0.] *)
+
+val cur_game : game
+(* current game the player is in*)
+
+val play_game : unit -> unit
+(* [run game] starts the game given [game]'s specifications *)
--- src/theme.mli
+++ src/theme.mli
@@ -9,8 +9,13 @@ type colors =
   | Z
   | J
   | L
+(* each color is a different part of the came that needs a distinct
+   color, i.e. tetrominoes and the background. *)
 
 val colors_of_pallete : Settings.palletes -> colors -> Graphics.color
+(** [colors_of_pallete ps c] outputs the color for piece c in pallete
+    ps. *)
 
 val theme_button :
   Settings.palletes -> int * int -> int * int -> Button.button
+(** [theme_button ps (i1, i2) (i1, i2) outputs a button that previews the theme \[ps\].] *)
