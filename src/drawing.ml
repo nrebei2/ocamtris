92ca697399e11b5dc781eae6f80c6921b1e41431
Jacobo Carreon <jic56@cornell.edu>
2021-12-12 15:53:55 -0500

    Refactoring Drawing.ml

--- src/drawing.ml
+++ src/drawing.ml
@@ -15,40 +15,31 @@ let draw_title () =
 
 let draw_next_piece board_pos =
   moveto
-    ((fst board_pos + 400) - (fst (text_size "Ocamtris") / 2))
-    ((snd board_pos + 500) - snd (text_size "Ocamtris"));
+    (fst board_pos + 400 - (fst (text_size "Ocamtris") / 2))
+    (snd board_pos + 500 - snd (text_size "Ocamtris"));
   draw_string "Next Piece"
+
+let draw_instructions_helper p font_siz str =
+  moveto
+    (fst p.board_pos + 350 - (fst (text_size "Ocamtris") / 2))
+    (font_siz - snd (text_size "Ocamtris"));
+  draw_string str
 
 (* [draw_instructions ()] draws the instructions for the game.*)
 let draw_instructions p =
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (350 - snd (text_size "Ocamtris"));
-  draw_string "Controls:";
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (325 - snd (text_size "Ocamtris"));
-  draw_string (Printf.sprintf "- Use \"%c\" and \"%c\" keys" p.controls.move_left p.controls.move_right);
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (310 - snd (text_size "Ocamtris"));
-  draw_string " to move left and right";
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (285 - snd (text_size "Ocamtris"));
-  draw_string (Printf.sprintf "- \"%c\" to rotate left" p.controls.rotate_left);
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (260 - snd (text_size "Ocamtris"));
-  draw_string (Printf.sprintf "- \"%c\" to drop isntantly" p.controls.drop);
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (235 - snd (text_size "Ocamtris"));
-  draw_string (Printf.sprintf "- \"%c\" to soft drop" p.controls.move_down);
-  moveto
-    ((fst p.board_pos + 350) - (fst (text_size "Ocamtris") / 2))
-    (210 - snd (text_size "Ocamtris"));
-  draw_string (Printf.sprintf "- \"%c\" to hold" p.controls.hold)
+  draw_instructions_helper p 350 "Controls:";
+  draw_instructions_helper p 325
+    (Printf.sprintf "- Use \"%c\" and \"%c\" keys" p.controls.move_left
+       p.controls.move_right);
+  draw_instructions_helper p 310 " to move left and right";
+  draw_instructions_helper p 285
+    (Printf.sprintf "- \"%c\" to rotate left" p.controls.rotate_left);
+  draw_instructions_helper p 260
+    (Printf.sprintf "- \"%c\" to drop isntantly" p.controls.drop);
+  draw_instructions_helper p 235
+    (Printf.sprintf "- \"%c\" to soft drop" p.controls.move_down);
+  draw_instructions_helper p 210
+    (Printf.sprintf "- \"%c\" to hold" p.controls.hold)
 
 (* [draw_outline p] draws the grid outline of the board to the GUI
    window, with its lower left position at [p]*)
@@ -61,7 +52,6 @@ let draw_outline board_pos =
   lineto
     (fst board_pos - 1)
     (snd board_pos + (tile_size * fst settings.board_size));
-
   (* right column *)
   moveto
     (fst board_pos + (tile_size * snd settings.board_size))
@@ -69,13 +59,11 @@ let draw_outline board_pos =
   lineto
     (fst board_pos + (tile_size * snd settings.board_size))
     (snd board_pos + (tile_size * fst settings.board_size));
-
   (* bottom row *)
   moveto (fst board_pos) (snd board_pos - 1);
   lineto
     (fst board_pos + (tile_size * snd settings.board_size))
     (snd board_pos - 1);
-
   (* top row *)
   moveto (fst board_pos)
     (snd board_pos + (tile_size * fst settings.board_size));
